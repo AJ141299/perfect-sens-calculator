7ee2ae230cbcfd73439e3dc62401fae669fd0ff0
Abdullah <abdullah141299@gmail.com>
2022-07-11 17:11:19 +0100

    adjusted mobile view for undo btn

--- src/app/components/floating-btn/floating-btn.component.ts
+++ src/app/components/floating-btn/floating-btn.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faUndo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-floating-btn',
@@ -9,6 +10,7 @@ export class FloatingBtnComponent implements OnInit {
   @Input() disable: boolean = true;
   @Input() text: string = '';
   @Output() clickedEvent = new EventEmitter();
+  faUndo = faUndo;
 
   constructor() {}
 
